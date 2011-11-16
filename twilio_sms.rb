b5f82ebc2e204f2b591289e79544872156546abb
Sam Gibson <sam@ifdown.net>
2011-11-16 18:28:22 +1100

    Add twilio sms... waiting on twilio to enable international SMS for us

---
+++ twilio_sms.rb
@@ -0,0 +1,16 @@
+require 'twilio-ruby'
+
+module ThoughtWorks
+  class SMS
+    def initialize(account_sid, auth_token)
+      @client = Twilio::REST::Client.new(account_sid, auth_token)
+      @account = @client.account
+    end
+
+    def send(number, message)
+      # The "from" number is our twilio number, this should probably be 
+      # changed once international SMS is enabled.
+      @account.sms.messages.create(:from => "+14155992671", :to => number, :body => message)
+    end
+  end
+end
