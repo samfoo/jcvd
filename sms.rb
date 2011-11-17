46bf5939979a65878e50158940f7fd5eb9000384
Sam Gibson <sam@ifdown.net>
2011-11-17 12:19:30 +1100

    Fixing SMS before we (hopefully) get international sms from twilio

--- sms.rb
+++ sms.rb
@@ -9,7 +9,6 @@ module ThoughtWorks
 
     def send(number, message)
       content = "m4u\r\nUSER=#{@user}#\r\nPASSWORD=#{@pass}\r\nVER=PHP1.0\r\nMESSAGES2.0\r\n0 #{number} 0 169 1 #{message}\r\n.\r\n"
-      puts content
       results = @agent.request_with_entity(:post, "http://smsmaster.m4u.com.au", content)
     end
   end
