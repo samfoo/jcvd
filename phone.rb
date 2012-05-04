e5960388a9b19a4f999a2d24319e16ea56bc663d
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2012-05-03 21:37:19 -0500

    Fixing a couple of bugs and providing the ability to run without actually sending anything. Also improving the phone regexp.

--- phone.rb
+++ phone.rb
@@ -2,13 +2,18 @@ require 'twilio-ruby'
 
 module ThoughtWorks
   class Phone
-    def initialize(account_sid, auth_token)
+    def initialize(account_sid, auth_token, dryrun)
       @client = Twilio::REST::Client.new(account_sid, auth_token)
       @account = @client.account
+      @dryrun = dryrun
     end
 
     def call(number, say)
-      @account.calls.create(:from => APP_CONFIG['twilio_number'], :to => number, :url => say)
+      if(@dryrun)
+        puts "Calling #{number} to play the sound available at #{say}"
+      else
+      	@account.calls.create(:from => APP_CONFIG['twilio_number'], :to => number, :url => say)
+      end
     end
   end
 end
--- twilio_sms.rb
+++ twilio_sms.rb
@@ -2,15 +2,21 @@ require 'twilio-ruby'
 
 module ThoughtWorks
   class SMS
-    def initialize(account_sid, auth_token)
+    def initialize(account_sid, auth_token, dryrun = false)
       @client = Twilio::REST::Client.new(account_sid, auth_token)
       @account = @client.account
+      @dryrun = dryrun
     end
 
     def send(number, message)
-      # The "from" number is our twilio number, this should probably be 
+      # The "from" number is our twilio number, this should probably be
       # changed once international SMS is enabled.
-      @account.sms.messages.create(:from => APP_CONFIG['twilio_number'], :to => number, :body => message)
+      if(@dryrun)
+        puts "Sending an SMS to #{number} with body:"
+        puts message
+      else
+        @account.sms.messages.create(:from => APP_CONFIG['twilio_number'], :to => number, :body => message)
+      end
     end
   end
 end
